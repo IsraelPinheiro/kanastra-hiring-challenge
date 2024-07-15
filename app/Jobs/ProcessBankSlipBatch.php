9399dadd44644b3d643ff20e07f1387ea1e5e59f
Israel Pinheiro <israel.pinheiro@live.com>
2024-07-15 07:13:08 -0300

    feat: Optimize bank slip batch processing and error handling

--- app/Jobs/ProcessBankSlipBatch.php
+++ app/Jobs/ProcessBankSlipBatch.php
@@ -6,7 +6,6 @@ use App\Enums\BankSlipBatchStatus;
 use App\Http\Traits\CsvParser;
 use App\Models\BankSlipBatch;
 use Illuminate\Bus\Batch;
-use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -19,9 +18,9 @@ use Illuminate\Support\Facades\Storage;
 
 class ProcessBankSlipBatch implements ShouldBeUnique, ShouldQueue
 {
-    use Batchable, CsvParser, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use CsvParser, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public function __construct(private BankSlipBatch $bankSlipBatch)
+    public function __construct(private BankSlipBatch $bankSlipBatch, private int $chunkSize = 1000)
     {
         $this->queue = 'bank_slip_batch_processing';
     }
@@ -33,6 +32,10 @@ class ProcessBankSlipBatch implements ShouldBeUnique, ShouldQueue
 
     public function handle(): void
     {
+        $bankSlipBatchID = $this->bankSlipBatch->id;
+
+        $this->bankSlipBatch->update(['processing_attempts' => $this->bankSlipBatch->processing_attempts + 1]);
+
         $csvData = $this->CsvToArray(
             Storage::disk(config('filesystems.default'))->get($this->bankSlipBatch->file_path)
         );
@@ -43,28 +46,36 @@ class ProcessBankSlipBatch implements ShouldBeUnique, ShouldQueue
             $csvData = collect($csvData)->filter(fn (array $row) => ! in_array($row['debtId'], $processedDebtIds))->toArray();
         }
 
-        Bus::batch(collect($this->CsvToArray(
-            Storage::disk(config('filesystems.default'))->get($this->bankSlipBatch->file_path)
-        ))->map(
-            fn (array $row) => new CreateBankSlip(
-                $row['name'],
-                $row['email'],
-                $row['governmentId'],
-                $row['debtAmount'],
-                $row['debtDueDate'],
-                $row['debtId']
-            )
-        ))->finally(function (Batch $batch) {
-            $this->bankSlipBatch->update([
-                'processing_attempts' => $this->bankSlipBatch->processing_attempts + 1,
-                'status' => $batch->hasFailures() ? BankSlipBatchStatus::Failed() : BankSlipBatchStatus::Processed(),
-            ]);
+        //Split the CSV data into chunks of rows to avoid memory issues
+        $chunks = array_chunk($csvData, $this->chunkSize);
 
-            if ($batch->hasFailures()) {
-                Log::error("Batch {$this->bankSlipBatch->id} - One or more bank slips failed to be created.");
-            }
-        })->name('Batch {$this->bankSlipBatch->id} processing')
-            ->allowFailures()
-            ->dispatch();
+        collect($chunks)->each(function (array $dataChunk, int $index) use ($bankSlipBatchID) {
+            Bus::batch(collect($dataChunk)->map(
+                fn (array $row) => new CreateBankSlip(
+                    $this->bankSlipBatch->id,
+                    $row['name'],
+                    $row['email'],
+                    $row['governmentId'],
+                    $row['debtAmount'],
+                    $row['debtDueDate'],
+                    $row['debtId']
+                )
+            ))->finally(function (Batch $batch) use ($bankSlipBatchID) {
+                $bankSlipBatch = BankSlipBatch::find($bankSlipBatchID);
+
+                if ($bankSlipBatch->status != BankSlipBatchStatus::Failed()) {
+                    $bankSlipBatch->update([
+                        'status' => $batch->hasFailures() ? BankSlipBatchStatus::Failed() : BankSlipBatchStatus::Processed(),
+                    ]);
+                }
+
+                if ($batch->hasFailures()) {
+                    Log::error("Batch {$bankSlipBatch->id} - One or more bank slips failed to be created.");
+                }
+            })->name('Processing Batch '.$this->bankSlipBatch->id.' - Chunk '.$index + 1)
+                ->allowFailures()
+                ->dispatch();
+        });
+
     }
 }
